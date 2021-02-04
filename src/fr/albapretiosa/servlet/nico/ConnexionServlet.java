2205ae9e4a9f368a263a9b1c682d76232bfc3dfd
dayuum <muscat13270@gmail.com>
2021-02-04 11:59:08 +0100

    C3.6 Modif ConnexionServet

--- src/fr/albapretiosa/servlet/nico/ConnexionServlet.java
+++ src/fr/albapretiosa/servlet/nico/ConnexionServlet.java
@@ -1,11 +1,19 @@
 package fr.albapretiosa.servlet.nico;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.albapretiosa.metier.nico.Abonne;
+import fr.albapretiosa.dao.dao;
+
 
 /**
  * Servlet implementation class ConnexionServlet
@@ -13,29 +21,42 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ConnexionServlet")
 public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnexionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	public ConnexionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		String context = request.getContextPath();
+		ArrayList<Abonne> abonnes = dao.abonnes;
+		Abonne abonneOk = new Abonne();
+		PrintWriter out = response.getWriter();
+		String alias = request.getParameter("alias");
+		String mdp = request.getParameter("mdp");
+		boolean connectionOk = false;
 
+		for (Abonne abonne : abonnes) {
+			if(alias.equals(abonne.getAlias()) && mdp.equals(abonne.getMdp())) {
+				connectionOk = true;
+				abonneOk = abonne;
+			}
+		}
+		if(connectionOk) {
+			out.println("Connection réussie "+alias);
+			HttpSession session = request.getSession(true);
+			session.setAttribute("Abonne", abonneOk);
+			response.sendRedirect(context+"/index.jsp");
+		}
+		else{
+			//		request.setAttribute("message", "Identifiants ou mot de passe incorrect.");
+			//		RequestDispatcher disp = request.getRequestDispatcher(Erreur.getErrorLocation());
+			//		disp.forward(request, response);
+			System.out.println("connexion echouée");
+		}
+	}
 }
