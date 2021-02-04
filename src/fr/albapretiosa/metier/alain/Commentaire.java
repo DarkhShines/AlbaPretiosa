16dab78eecc33b636d81f55185a348bb5902d3ae
DarkhShines <dardotalain@gmail.com>
2021-02-04 09:53:22 +0100

    Commentaire.java ajouté

---
+++ src/fr/albapretiosa/metier/alain/Commentaire.java
@@ -0,0 +1,10 @@
+package fr.albapretiosa.metier.alain;
+
+import java.time.LocalDate;
+
+public class Commentaire {
+	private String idCom;
+	private String commentaire;
+	private LocalDate dateCom;
+	
+}
