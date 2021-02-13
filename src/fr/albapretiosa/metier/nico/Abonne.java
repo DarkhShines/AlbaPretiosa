8ee40eda1709b85d55af50f0a663efe0031f6093
dayuum <muscat13270@gmail.com>
2021-02-13 18:12:24 +0100

    Ajout methode de controle dans Abonne

--- src/fr/albapretiosa/metier/nico/Abonne.java
+++ src/fr/albapretiosa/metier/nico/Abonne.java
@@ -8,7 +8,7 @@ import fr.albapretiosa.servlet.nico.InscriptionServlet;
 public class Abonne {
 	// PROPRIETE DE CLASSE
 	protected static AtomicInteger genId = new AtomicInteger(1);
-	
+
 	// PROPRIETE D'INSTANCE
 	private String 	nom;
 	private int 	idAbonne;
@@ -20,18 +20,18 @@ public class Abonne {
 	private boolean platinum;
 	private String	mdp;
 	private String 	parrainage; 
-	
+
 	// CONSTRUCTEUR 
 	public Abonne(String nom, String prenom, String alias, String email, String telPortable, String mdp, String parrainage) {
 		super();
 		setNom(nom);
 		setIdAbonne(genId.getAndIncrement()); 
 		setPrenom(prenom);
-	    setAlias(alias);
-	    setEmail(email);
-	    setTelPortable(telPortable);
-	    setMdp(mdp);
-	    setParrainage(parrainage);
+		setAlias(alias);
+		setEmail(email);
+		setTelPortable(telPortable);
+		setMdp(mdp);
+		setParrainage(parrainage);
 	}
 	public Abonne(String nom, String prenom, String alias, String email, String telPortable,
 			String telFixe, String mdp, String parrainage){
@@ -39,17 +39,17 @@ public class Abonne {
 		setNom(nom);
 		setIdAbonne(genId.getAndIncrement()); 
 		setPrenom(prenom);
-	    setAlias(alias);
-	    setEmail(email);
-	    setTelPortable(telPortable);
-	    setTelFixe(telFixe);
-	    setMdp(mdp);
-	    setParrainage(parrainage);
+		setAlias(alias);
+		setEmail(email);
+		setTelPortable(telPortable);
+		setTelFixe(telFixe);
+		setMdp(mdp);
+		setParrainage(parrainage);
 	}
 	public Abonne() {
 		super();
 	}
-	
+
 	// GETTER
 	public String getNom() {
 		return nom;
@@ -81,7 +81,7 @@ public class Abonne {
 	public String getParrainage() {
 		return parrainage;
 	}
-	
+
 	// SETTER
 	public void setNom(String nom) {
 		this.nom = nom;
@@ -115,7 +115,7 @@ public class Abonne {
 	public void setParrainage(String parrainage) {
 		this.parrainage = parrainage;
 	}
-	
+
 	// METHODE 
 	@Override
 	public String toString() {
@@ -123,19 +123,56 @@ public class Abonne {
 				+ email + ", telPortable=" + telPortable + ", telFixe=" + telFixe + ", platinum=" + platinum + ", mdp="
 				+ mdp + ", parrainage=" + parrainage + "]";
 	}
-	
+
 	// METHODE CONTROLE SAISIE
-		 public void controleNom(String nom ) throws Exception_Nico {
-		        if( nom == null || nom.trim() == "") {
-		            throw new Exception_Nico( "Le Nom n'est pas renseigné" );
-		        }
-		        if ( nom != null && nom.trim().length() < 2 ) {
-		            throw new Exception_Nico( "Le Nom doit contenir au moins 2 caractères." );
-		        }
-		        if ( nom != null && nom.trim().length() > 15 ) {
-		            throw new Exception_Nico( "Le titre ne peut contenir plus de 15 caractères." );
-		        }
+
+	public void controleNom(String nom ) throws Exception_Nico {
+		if( nom == null || nom.trim() == "") {
+			throw new Exception_Nico( "Le nom n'est pas renseigné" );
 		}
-	
-	
+		if ( nom != null && nom.trim().length() < 2 ) {
+			throw new Exception_Nico( "Le nom doit contenir au moins 2 caractères." );
+		}
+		if ( nom != null && nom.trim().length() > 15 ) {
+			throw new Exception_Nico( "Le nom ne peut contenir plus de 15 caractères." );
+		}
+	}
+	public void controlePrenom(String Prenom ) throws Exception_Nico {
+		if( Prenom == null || Prenom.trim() == "") {
+			throw new Exception_Nico( "Le prenom n'est pas renseigné" );
+		}
+		if ( Prenom != null && Prenom.trim().length() < 2 ) {
+			throw new Exception_Nico( "Le prenom doit contenir au moins 2 caractères." );
+		}
+		if ( Prenom != null && Prenom.trim().length() > 15 ) {
+			throw new Exception_Nico( "Le prenom ne peut contenir plus de 15 caractères." );
+		}
+	}
+	public void controleAlias(String Alias ) throws Exception_Nico {
+		if( Alias == null || Alias.trim() == "") {
+			throw new Exception_Nico( "L'alias n'est pas renseigné" );
+		}
+		if ( Alias != null && Alias.trim().length() < 2 ) {
+			throw new Exception_Nico( "L'alias doit contenir au moins 2 caractères." );
+		}
+		if ( Alias != null && Alias.trim().length() > 15 ) {
+			throw new Exception_Nico( "L'alias ne peut contenir plus de 15 caractères." );
+		}
+	}
+	public void controleTelportable(String telPortable ) throws Exception_Nico {
+		if( telPortable == null || telPortable.trim() == "") {
+			throw new Exception_Nico( "Le téléphone Portable n'est pas renseigné" );
+		}
+		if ( telPortable != null && telPortable.trim().length() != 10 ) {
+			throw new Exception_Nico( "Le téléphone Portable doit contenir 10 caractères." );
+		}
+	}
+	public void controletelFixe(String telFixe ) throws Exception_Nico {
+		if( telFixe == null || telFixe.trim() == "") {
+			throw new Exception_Nico( "Le téléphone Fixe n'est pas renseigné" );
+		}
+		if ( telFixe != null && telFixe.trim().length() != 10 ) {
+			throw new Exception_Nico( "Le téléphone Fixe doit contenir 10 caractères." );
+		}
+	}
 }
