23acaa919140a4ceadc0e3f240f4cd17ad30e761
DarkhShines <dardotalain@gmail.com>
2021-02-23 15:09:00 +0100

    ytytyt

---
+++ src/fr/albapretiosa/dao/ConstRequest.java
@@ -0,0 +1,10 @@
+package fr.albapretiosa.dao;
+
+/**
+ * @author Alain
+ * @category Classe de manipulation de requetes SQL. 
+ *
+ */
+public class ConstRequest {
+		protected static final String test = "";
+}
